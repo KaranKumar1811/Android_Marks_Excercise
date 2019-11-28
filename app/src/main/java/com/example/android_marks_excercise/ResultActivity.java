e0f6c28cc44089422ae2173d9d40ad52ff335174
Karan <kumarkay.ca@gmail.com>
2019-11-27 19:57:29 -0500

    sending data from 1 to another Screen

--- app/src/main/java/com/example/android_marks_excercise/ResultActivity.java
+++ app/src/main/java/com/example/android_marks_excercise/ResultActivity.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.Arrays;
+
 public class ResultActivity extends AppCompatActivity {
 
-    Marks marks=new Marks();
-    TextView iosMarks,androidMarks,javaMarks,dbmsMarks,swiftMarks;
+    Marks marks;
+    Marks m ;
+    TextView iosMarks,androidMarks,javaMarks,dbmsMarks,swiftMarks,showres;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,18 +23,42 @@ public class ResultActivity extends AppCompatActivity {
         javaMarks=findViewById(R.id.mJavaView);
         dbmsMarks=findViewById(R.id.mDbmsView);
         swiftMarks=findViewById(R.id.mSwiftView);
+        showres=findViewById(R.id.calculationShow);
 
 
-
-        Marks m = (Marks) getIntent().getSerializableExtra("marks");
-
+        //Marks m = (Marks) getIntent().getSerializableExtra("marks");
+    m = (Marks) getIntent().getSerializableExtra("marks");
 
     iosMarks.setText("iOS : "+String.valueOf(m.getmIos()));
     androidMarks.setText("Android : "+String.valueOf(m.getmAndroid()));
     javaMarks.setText(String.valueOf("Java : "+m.getmJava()));
     swiftMarks.setText(String.valueOf("Swift : "+m.getmSwift()));
     dbmsMarks.setText(String.valueOf("DBMS : "+m.getmDBMS()));
+    showres.setText("Max : "+ max()+" Min : "+min()+" Avg : "+avg());
 
 
+
+
+    }
+
+public int max(){
+        int[] nums={m.getmSwift(),m.getmIos(),m.getmDBMS(),m.getmAndroid(),m.getmJava()};
+    Arrays.sort(nums);
+        return nums[0];
+}
+
+    public int min(){
+        int[] num={m.getmSwift(),m.getmIos(),m.getmDBMS(),m.getmAndroid(),m.getmJava()};
+        Arrays.sort(num);
+        return num[num.length-1];
+    }
+    public int avg(){
+        int[] num={m.getmSwift(),m.getmIos(),m.getmDBMS(),m.getmAndroid(),m.getmJava()};
+        int sum=0;
+        for(int i=0;i<num.length;i++)
+        {
+            sum=sum+num[i];
+        }
+        return sum/num.length;
     }
 }
